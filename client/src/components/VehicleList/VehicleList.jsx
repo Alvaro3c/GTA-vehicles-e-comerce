537ca22522e95bea9e189f679f4ea681e914ba41
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-07-15 17:24:27 +0200

    FIX/sort buttons stoped working, now it works.

--- client/src/components/VehicleList/VehicleList.jsx
+++ client/src/components/VehicleList/VehicleList.jsx
@@ -1,130 +1,142 @@
 import React, { useEffect, useState } from "react";
-import Vehicle from '../Vehicle/Vehicle'
-import Order from '../Order/Order'
-
+import Vehicle from '../Vehicle/Vehicle';
 
 const VehicleList = (props) => {
   const search = props.search;
   const [vehicles, setVehicles] = useState([]);
+  const [sortedVehicles, setSortedVehicles] = useState([]);
   const [vehiclesSliced, setVehiclesSliced] = useState([]);
-  const [filterVehicles, setFilterVehicles] = useState([]);
-  const [selectedType, setSelectedType] = useState('suvs')
-  const [sortAtoZ, setSortAtoZ] = useState(true)
-  const [selectedPage, setSelectedPage] = useState(1)
+  const [selectedType, setSelectedType] = useState('suvs');
+  const [sortAtoZ, setSortAtoZ] = useState(true);
+  const [selectedPage, setSelectedPage] = useState(1);
   const itemsPerPage = 10;
 
-
   const handleSelectChange = (e) => {
-    e.preventDefault()
-    setSelectedType(e.target.value)
-  }
+    e.preventDefault();
+    setSelectedType(e.target.value);
+    setSortAtoZ(false); // Set the default sorting order to ascending (A to Z)
+  };
 
-  //Seacrh vehicle
   useEffect(() => {
     if (search === '') {
       getVehicles();
     } else {
-      const filteredVehicles = vehicles.filter((item) => item.manufacturer.includes(search) || item.model.includes(search))
-      setVehicles(filteredVehicles)
+      const filteredVehicles = vehicles.filter((item) =>
+        item.manufacturer.includes(search) || item.model.includes(search)
+      );
+      setVehicles(filteredVehicles);
     }
-    setSelectedPage(1)
-    handlePageChange(1)
-  }, [search, selectedType])
+    setSelectedPage(1);
+    handlePageChange(1);
+  }, [search, selectedType]);
 
-  //fetch vehicles
   const getVehicles = async () => {
     const resp = await fetch(`https://gta.vercel.app/api/vehicles/class/${selectedType}`);
     const data = await resp.json();
-
-    const arrVehicles = Object.values(data);//converts an object to an array to be iterated 
+    const arrVehicles = Object.values(data);
     setVehicles(arrVehicles);
-    const slicedData = arrVehicles.slice(0, 10);
+    handleSortAndPageChange(arrVehicles);
+  };
+
+  const handleSortAndPageChange = (sortedVehiclesData) => {
+    const slicedData = sortedVehiclesData.slice(0, itemsPerPage);
+    setSortedVehicles(sortedVehiclesData);
     setVehiclesSliced(slicedData);
-  }
+    setSelectedPage(1);
+  };
 
   const handleSortSpeed = () => {
-    const sortedVehicles = vehicles
-      .map((vehicle, index) => ({ index, mph: vehicle?.topSpeed?.mph }))
-      .sort((a, b) => b.mph - a.mph)
-      .map((item) => vehicles[item.index]);
-    setVehicles(sortedVehicles);
+    const sortedVehiclesData = [...vehicles].sort((a, b) => b?.topSpeed?.mph - a?.topSpeed?.mph);
+    handleSortAndPageChange(sortedVehiclesData);
   };
+
   const handleSortAlphabetically = () => {
-    if (sortAtoZ) {
-      const sortedVehicles = [...vehicles].sort((a, b) => a.model.localeCompare(b.model));
-      setVehicles(sortedVehicles);
-    } else {
-      const sortedVehicles = [...vehicles].sort((a, b) => b.model.localeCompare(a.model));
-      setVehicles(sortedVehicles);
-    }
-    setSortAtoZ(!sortAtoZ)
+    const sortedVehiclesData = [...vehicles].sort((a, b) => {
+      const modelA = a.model || '';
+      const modelB = b.model || '';
+      return sortAtoZ ? modelA.localeCompare(modelB) : modelB.localeCompare(modelA);
+    });
+    setSortAtoZ(!sortAtoZ);
+    handleSortAndPageChange(sortedVehiclesData);
   };
 
   const handlePageChange = (page) => {
-    setSelectedPage(page)
-    // Calculate start and end indices
+    setSelectedPage(page);
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = page * itemsPerPage;
-
-    // Slice the data array based on the calculated indices
-    const slicedData = vehicles.slice(startIndex, endIndex);
+    const slicedData = sortedVehicles.slice(startIndex, endIndex);
     setVehiclesSliced(slicedData);
-  }
+  };
 
   const renderPagination = () => {
-    const pageNumbers = []
+    const pageNumbers = [];
+    const totalPages = Math.ceil(sortedVehicles.length / itemsPerPage);
 
-    for (let i = 1; i <= Math.ceil(vehicles.length / itemsPerPage); i++) {
-      pageNumbers.push(i)
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
     }
 
     return (
       <div>
         <button
-
+          disabled={selectedPage === 1}
           onClick={() => handlePageChange(selectedPage - 1)}
         >
           <span aria-hidden="true">&laquo;</span>
           <span className="sr-only">Back</span>
         </button>
-        {pageNumbers?.map((pageNumber, index) =>
-          <li key={index}>
-            <button disabled={selectedPage == pageNumber} onClick={() => handlePageChange(pageNumber)}>
-              {pageNumber}
-            </button>
-          </li>
-        )}
+        {pageNumbers.map((pageNumber) => (
+          <button
+            key={pageNumber}
+            disabled={selectedPage === pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        ))}
         <button
-
+          disabled={selectedPage === totalPages}
           onClick={() => handlePageChange(selectedPage + 1)}
         >
           <span aria-hidden="true">&raquo;</span>
           <span className="sr-only">Next</span>
         </button>
       </div>
-    )
-  }
-  return <>
+    );
+  };
 
-    <label htmlFor="select-car-type">Select a type</label>
-    <select name="" id="select-car-type" onChange={handleSelectChange}>
-      <option value="suvs">suvs</option>
-      <option value="super">super</option>
-      <option value="sportsclassics">sportsclassics</option>
-      <option value="sports">sports</option>
-      <option value="sedans">sedans</option>
-      <option value="offroad">offroad</option>
-      <option value="motorcycles">motorcycles</option>
-      <option value="coupes">coupes</option>
-      <option value="muscle">muscle</option>
-      <option value="compacts">compacts</option>
-    </select>
-    <button onClick={handleSortSpeed}>Sort with Speed</button>
-    <button onClick={handleSortAlphabetically}>Sort with alphebet</button>
-    {renderPagination()}
-    {vehiclesSliced && vehiclesSliced.map((item, i) => <Vehicle key={i} manufacturer={item.manufacturer} model={item.model} price={item.price} imgUrl={item.images.frontQuarter} shopingCart={props.shopingCart} setShopingCart={props.setShopingCart} />)}
-
-  </>;
+  return (
+    <>
+      <label htmlFor="select-car-type">Select a type</label>
+      <select name="" id="select-car-type" onChange={handleSelectChange}>
+        <option value="suvs">suvs</option>
+        <option value="super">super</option>
+        <option value="sportsclassics">sportsclassics</option>
+        <option value="sports">sports</option>
+        <option value="sedans">sedans</option>
+        <option value="offroad">offroad</option>
+        <option value="motorcycles">motorcycles</option>
+        <option value="coupes">coupes</option>
+        <option value="muscle">muscle</option>
+        <option value="compacts">compacts</option>
+      </select>
+      <button onClick={handleSortSpeed}>Sort by Speed</button>
+      <button onClick={handleSortAlphabetically}>Sort Alphabetically</button>
+      {renderPagination()}
+      {vehiclesSliced &&
+        vehiclesSliced.map((item, i) => (
+          <Vehicle
+            key={i}
+            manufacturer={item.manufacturer}
+            model={item.model}
+            price={item.price}
+            imgUrl={item.images.frontQuarter}
+            shopingCart={props.shopingCart}
+            setShopingCart={props.setShopingCart}
+          />
+        ))}
+    </>
+  );
 };
 
 export default VehicleList;
