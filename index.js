d64493f87ed9dea74d1cb058c44b612da3adac53
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-09-18 13:30:33 +0200

    add/ port is in .env

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const error404 = require('./middlewares/error404');
 const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
 
 //Módulos de rutas
 const ordersApiRoutes = require('./routes/ordersApiRoutes')
@@ -21,6 +22,6 @@ app.use('/api/orders', ordersApiRoutes);
 app.use("/api/users", usersApiRoutes);
 app.use(error404);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Example app listening on port 3000!');
 });
